73d5da05cce53e29cdd3d8f07962a1551c23b472
HeteroChromia420 <56137935+HeteroChromia420@users.noreply.github.com>
2024-06-13 20:50:55 +0100

    Update postsetup.sh

--- postsetup.sh
+++ postsetup.sh
@@ -5,3 +5,4 @@ sudo apk del firefox
 #Please read on https://wiki.alpinelinux.org/wiki/Xfce if you need something this script doesn't add.
 sudo apk add adw-gtk3 adwaita-xfce-icon-theme udisks2 gnome-disk-utility dbus polkit xdg-desktop-portal xdg-desktop-portal-gtk pipewire pipewire-pulse pipewire-alsa wireplumber bash bash-completion xfce4-pulseaudio-plugin xfce4-whiskermenu-plugin network-manager-applet elogind polkit-elogind xfce-polkit xfce4-screensaver pavucontrol mousepad discover discover-backend-flatpak discover-backend-apk kcmutils kirigami kirigami-addons kitemmodels xarchiver
 flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+xdg-user-dirs-update
