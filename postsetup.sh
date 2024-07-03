ab466a196be00b74ba754857b30247c05dae3457
HeteroChromia420 <56137935+HeteroChromia420@users.noreply.github.com>
2024-07-03 13:50:18 +0100

    Update postsetup.sh

--- postsetup.sh
+++ postsetup.sh
@@ -3,6 +3,6 @@
 sudo apk del firefox
 #Removing Firefox as it'll be replaced with the flatpak version.
 #Please read on https://wiki.alpinelinux.org/wiki/Xfce if you need something this script doesn't add.
-sudo apk add adw-gtk3 adwaita-xfce-icon-theme udisks2 gnome-disk-utility dbus polkit xdg-desktop-portal xdg-desktop-portal-gtk pipewire pipewire-pulse pipewire-alsa wireplumber bash bash-completion xfce4-pulseaudio-plugin xfce4-whiskermenu-plugin network-manager-applet elogind polkit-elogind xfce-polkit xfce4-screensaver pavucontrol mousepad discover discover-backend-flatpak discover-backend-apk kcmutils kirigami kirigami-addons kitemmodels xarchiver flatpak xdg-user-dirs xfce4-notifyd font-noto-all gcompat 
+sudo apk add adw-gtk3 adwaita-xfce-icon-theme udisks2 gnome-disk-utility dbus polkit xdg-desktop-portal xdg-desktop-portal-gtk pipewire pipewire-pulse pipewire-alsa wireplumber bash bash-completion xfce4-pulseaudio-plugin xfce4-whiskermenu-plugin network-manager-applet elogind polkit-elogind xfce-polkit xfce4-screensaver pavucontrol mousepad discover discover-backend-flatpak discover-backend-apk kcmutils kirigami kirigami-addons kitemmodels xarchiver flatpak xdg-user-dirs xfce4-notifyd gcompat util-linux pciutils hwdata-pci usbutils hwdata-usb coreutils binutils findutils grep iproute2 bash bash-completion font-terminus font-inconsolata font-dejavu font-noto font-noto-cjk font-awesome font-noto-extra
 flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 xdg-user-dirs-update
